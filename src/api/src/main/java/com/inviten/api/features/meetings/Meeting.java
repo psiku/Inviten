dd052b99687bf8104360d8addd29ef081a8fc988
ajonczyk <01178532@pw.edu.pl>
2023-11-26 01:44:48 +0100

    Implemented Meeting class

--- src/api/src/main/java/com/inviten/api/features/meetings/Meeting.java
+++ src/api/src/main/java/com/inviten/api/features/meetings/Meeting.java
@@ -2,10 +2,18 @@ package com.inviten.api.features.meetings;
 
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import java.util.Date;
+import java.util.List;
+import java.time.LocalTime;
+
 
 @DynamoDbBean
 public class Meeting {
     private String id;
+    private Date date;
+    private LocalTime time;
+    private List<String> participants;
+    // String change to object user
 
     @DynamoDbPartitionKey
     public String getId() {
@@ -15,4 +23,22 @@ public class Meeting {
     public void setId(String id) {
         this.id = id;
     }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {this.date = date;}
+
+    public LocalTime getTime() {
+        return time;
+    }
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public List<String> getParticipants() {return participants;}
+    public void setParticipants(List<String> participants) {
+        this.participants = participants;
+    }
+
 }
