7c384e75e70571dff15e8a5b7063be4211bd73e3
ajonczyk <01178532@pw.edu.pl>
2023-11-26 01:51:25 +0100

    Implemented User class - getters and setters for id and nick

---
+++ src/api/src/main/java/com/inviten/api/features/shared/User.java
@@ -0,0 +1,23 @@
+package com.inviten.api.features.shared;
+
+public class User {
+    private String id;
+    private String nick;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+}
