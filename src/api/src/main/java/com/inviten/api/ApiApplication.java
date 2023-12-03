3220150fdf726d4610e4da622a0a7456e0117988
Bartosz <01178579@pw.edu.pl>
2023-12-04 00:10:26 +0100

    Swagger configuration

--- src/api/src/main/java/com/inviten/api/ApiApplication.java
+++ src/api/src/main/java/com/inviten/api/ApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @SpringBootApplication
 @RestController
 public class ApiApplication {
