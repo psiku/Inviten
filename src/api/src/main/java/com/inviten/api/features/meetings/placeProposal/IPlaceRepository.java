4361501218a03cca2ae4b8e6ee473a93a9ae53dd
Bartosz <01178579@pw.edu.pl>
2024-01-12 14:16:20 +0100

    change Placeproposal

--- src/api/src/main/java/com/inviten/api/features/meetings/placeProposal/IPlaceRepository.java
+++ src/api/src/main/java/com/inviten/api/features/meetings/placeProposal/IPlaceRepository.java
@@ -1,13 +1,13 @@
 package com.inviten.api.features.meetings.placeProposal;
 
 public interface IPlaceRepository {
-    public void addPlaceProposal(Place place, String meetingId);
+    public Place addPlaceProposal(Place place, String meetingId);
 
     public void removePlaceProposal(Place place, String meetingId);
 
-    public void addVote(String meetingId, String placeId);
+    public Place addVote(String meetingId, String placeId);
 
     public void removeVote(String meetingId, String placeId);
 
-    public void confirmPlace(String meetingId, String proposalId);
+    public Place confirmPlace(String meetingId, String proposalId);
 }
--- src/api/src/main/java/com/inviten/api/features/meetings/placeProposal/PlaceRepository.java
+++ src/api/src/main/java/com/inviten/api/features/meetings/placeProposal/PlaceRepository.java
@@ -21,7 +21,7 @@ public class PlaceRepository implements IPlaceRepository{
     }
 
     @Override
-    public void addPlaceProposal(Place place, String id) {
+    public Place addPlaceProposal(Place place, String id) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         String phoneNumber = (String) authentication.getPrincipal();
@@ -29,7 +29,7 @@ public class PlaceRepository implements IPlaceRepository{
 
         Meeting meeting = meetingRepository.one(id);
         if(meeting.getIsPlaceChosen()){
-            return;
+            return null;
         }
         List<Place> places = meeting.getPlaceProposals();
         if(places == null){
@@ -40,6 +40,7 @@ public class PlaceRepository implements IPlaceRepository{
         }
         meeting.setPlaceProposals(places);
         meetingRepository.put(meeting);
+        return place;
     }
 
     @Override
@@ -71,25 +72,35 @@ public class PlaceRepository implements IPlaceRepository{
     }
 
     @Override
-    public void addVote(String meetingId, String placeId) {
+    public Place addVote(String meetingId, String placeId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         String phoneNumber = (String) authentication.getPrincipal();
 
         Meeting meeting = meetingRepository.one(meetingId);
         if(meeting.getIsPlaceChosen()){
-            return;
+            return null;
         }
         List<Place> places = meeting.getPlaceProposals();
         if(places == null) {
             List<Place> newPlaces = new ArrayList<>();
             places = newPlaces;
         }
-        Iterator<Place> iterator = places.iterator();
-        while (iterator.hasNext()) {
-            Place currentPlace = iterator.next();
-            if (currentPlace.getId().equals(placeId)) {
-                List<String> votes = currentPlace.getVotes();
+
+        // szukamy place
+        int indexOfPlace = -1;
+        for(int i = 0; i < places.size(); i++){
+            if(places.get(i).getId().equals(placeId)){
+                indexOfPlace = i;
+                break;
+            }
+        }
+
+        // pobierz ten place
+        Place currentPlace = places.get(indexOfPlace);
+
+        // pobierz listę głosów
+        List<String> votes = currentPlace.getVotes();
                 if(votes == null)
                 {
                     votes = List.of(phoneNumber);
@@ -98,12 +109,27 @@ public class PlaceRepository implements IPlaceRepository{
                     votes.add(phoneNumber);
                 }
                 currentPlace.setVotes(votes);
-                break;
-            }
-        }
+
+//        Iterator<Place> iterator = places.iterator();
+//        while (iterator.hasNext()) {
+//            Place currentPlace = iterator.next();
+//            if (currentPlace.getId().equals(placeId)) {
+//                List<String> votes = currentPlace.getVotes();
+//                if(votes == null)
+//                {
+//                    votes = List.of(phoneNumber);
+//                }
+//                else{
+//                    votes.add(phoneNumber);
+//                }
+//                currentPlace.setVotes(votes);
+//                break;
+//            }
+//        }
         sortPlaces(meeting);
         meeting.setPlaceProposals(places);
         meetingRepository.put(meeting);
+        return currentPlace;
     }
 
     @Override
@@ -143,17 +169,22 @@ public class PlaceRepository implements IPlaceRepository{
     }
 
     @Override
-    public void confirmPlace(String meetingId, String proposalId){
+    public Place confirmPlace(String meetingId, String proposalId){
         Meeting meeting = meetingRepository.one(meetingId);
-        List<Place> placeProposals = meeting.getPlaceProposals();
-        for(Place proposal: placeProposals){
-            if(proposal.getId().equals(proposalId)){
-                meeting.setPlace(proposal);
+        List<Place> places = meeting.getPlaceProposals();
+        int indexOfPlace = -1;
+        for(int i = 0; i < places.size(); i++){
+            if(places.get(i).getId().equals(proposalId)){
+                indexOfPlace = i;
                 break;
             }
         }
+        // pobierz ten place
+        Place currentPlace = places.get(indexOfPlace);
+        meeting.setPlace(currentPlace);
         meeting.setIsPlaceChosen(true);
         meetingRepository.put(meeting);
+        return currentPlace;
     }
 
 }
